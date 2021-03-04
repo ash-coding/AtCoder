93ec9fd9477f28c4d37f9e11aab26a2ec32ef137
ash-coding <64770032+ash-coding@users.noreply.github.com>
2021-03-05 00:49:34 +0900

    modify helloWorld

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,12 @@
-import math
+"""
+Python3
+Atcoder
+"""
 
 s1 = "Hello"
 s2 = "World"
+s3 = "AtCoder"
+s4 = "Python"
 print(s1 + " " + s2 + "!")
-
+print(s1 + " " + s3 + "!")
+print(s1 + " " + s4 + "!")
